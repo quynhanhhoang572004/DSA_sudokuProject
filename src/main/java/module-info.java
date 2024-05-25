b8e76f8ae2c332cc9b09b8eba119a59160b0cc02
quynhanhhoang572004 <quynhanhhoang572004@gmail.com>
2024-05-26 01:00:45 +0700

    add initial

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.sudokugame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.example.sudokugame to javafx.fxml;
-    exports org.example.sudokugame;
-}
