7eb3dc75dfd1e53eeb0d4ceda5590e996df0b87b
quynhanhhoang572004 <quynhanhhoang572004@gmail.com>
2024-06-14 21:26:10 +0700

    adding comment for game

--- src/main/java/Controllers/App.java
+++ src/main/java/Controllers/App.java
@@ -1,8 +1,12 @@
-package Controllers;
 /* Name: Hoang Ngoc Quynh Anh
- Purpose:.
+ Purpose: This game, called Soduku (female edition), includes five levels: easy, medium, hard, expert, and evil.
+ The 6x6 sodoku game's easy and medium levels require players to fill in each row and column from 1 to 6.
+ This level is intended for novices and features a smaller grid that is simpler to understand and solve.
+ If you're new to Sudoku or just want a short and entertaining challenge, this is ideal for you.
+ On the other hand, level hard, expert, evil is a 9x9 sodoku game designed for players who want to push their mental
+ limits and find it somewhat more difficult to complete the puzzles. Players must fill each row and column from 1 to 9.
 */
-
+package Controllers;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,7 +20,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/static/MainPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 604, 591);
-        stage.setTitle("Soduku Girls edition");
+        stage.setTitle("Soduku (Girls edition)");
         stage.setResizable(true);
         stage.setScene(scene);
         navigationManager.initialize(stage);
